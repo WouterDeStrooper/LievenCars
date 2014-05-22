286c714c6eb4f4a00d809299c8bf51d70ab2d667
Wouter De Strooper <wouterdestrooper@msn.com>
2014-05-22 15:39:45 +0200

    Updated database, Added ef of db to project

--- LievenCars/Filters/InitializeSimpleMembershipAttribute.cs
+++ LievenCars/Filters/InitializeSimpleMembershipAttribute.cs
@@ -38,7 +38,7 @@ namespace LievenCars.Filters
                         }
                     }
 
-                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "User", "UserId", "UserName", autoCreateTables: true);
                 }
                 catch (Exception ex)
                 {
